608e83fe3085b4a883f2333b8eb661427ab5fd45
María Cecilia <lunama.cecilia@live.com>
2025-12-25 17:48:11 -0300

    agrego page de FAQs

---
+++ src/pages/FAQs.jsx
@@ -0,0 +1,9 @@
+function FAQs() {
+  return (
+    <div>
+      <h1>Preguntas frecuentes</h1>
+    </div>
+  );
+}
+
+export default FAQs;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,6 +6,7 @@ import ProtectedRoute from './ProtectedRoute';
 // Páginas públicas
 import Inicio from '../pages/Inicio';
 import PaginaError from '../pages/PaginaError';
+import FAQs from '../pages/FAQs';
 
 // Feature: Autenticación
 import { PaginaLogin, PaginaRegistro, PaginaRecuperacion } from '../features/autenticacion';
@@ -27,6 +28,7 @@ function AppRoutes() {
         <Route path="/recuperar-contrasena" element={<PaginaRecuperacion />} />
         <Route path="/error" element={<PaginaError />} />
         <Route path="*" element={<Navigate to="/error" replace />} />
+        <Route path="/faqs" element={<FAQs />} />
       </Route>
 
       {/* Rutas Privadas */}
